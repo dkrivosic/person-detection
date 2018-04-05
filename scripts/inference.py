d3ad11949fe0b8779337cb04d4c1f4cb70edeec2
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-04-05 10:38:44 +0200

    Add inference script

---
+++ scripts/inference.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+import os
+import numpy as np
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+def run_inference_for_single_image(image, graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+            # Get handles to input and output tensors
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in [
+              'num_detections', 'detection_boxes', 'detection_scores',
+              'detection_classes', 'detection_masks'
+            ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+            if 'detection_masks' in tensor_dict:
+                # The following processing is only for single image
+                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                detection_masks_reframed = tf.cast(
+                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                # Follow the convention by adding back the batch dimension
+                tensor_dict['detection_masks'] = tf.expand_dims(
+                    detection_masks_reframed, 0)
+            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+            # Run inference
+            output_dict = sess.run(tensor_dict,
+                                 feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+        # all outputs are float32 numpy arrays, so convert types as appropriate
+        output_dict['num_detections'] = int(output_dict['num_detections'][0])
+        output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+        if 'detection_masks' in output_dict:
+            output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+
+
+flags = tf.app.flags
+flags.DEFINE_string('model_path', '',
+                    'Path to a frozen tensorflow model.')
+flags.DEFINE_string('label_map_path', '',
+                    'Path to the label map.')
+flags.DEFINE_string('test_images_dir', '',
+                    'Path to the directory with test images.')
+flags.DEFINE_string('output_dir', '',
+                    'Path to the output directory.')
+FLAGS = flags.FLAGS
+
+# Load Tensorflow model into memory
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(FLAGS.model_path, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+print('Model loaded successfully.')
+
+# Load label map
+label_map = label_map_util.load_labelmap(FLAGS.label_map_path)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+print('Label map loaded.')
+
+
+PATH_TO_TEST_IMAGES = FLAGS.test_images_dir
+TEST_IMAGE_PATHS = sorted([os.path.join(PATH_TO_TEST_IMAGES, image_name) for image_name in os.listdir(PATH_TO_TEST_IMAGES) if '.png' in image_name])
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+if not os.path.exists(FLAGS.output_dir):
+    os.makedirs(FLAGS.output_dir)
+
+# Inference
+for image_path in TEST_IMAGE_PATHS:
+    print('Running inference for ' + image_path)
+    image = Image.open(image_path).convert('RGB')
+    image_np = load_image_into_numpy_array(image)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    output_dict = run_inference_for_single_image(image_np, detection_graph)
+
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    plt.figure(figsize=IMAGE_SIZE)
+    plt.imshow(image_np)
+
+    image_name = image_path.split('/')[-1]
+    output_path = os.path.join(FLAGS.output_dir, image_name)
+    plt.savefig(output_path)
