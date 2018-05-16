a891c910d2056195e0846ae6691c57b84ed9fb13
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-05-16 15:19:05 +0200

    Change the script to work with negative examples that do not have xml files

--- scripts/generate_records_people_art.py
+++ scripts/generate_records_people_art.py
@@ -1,13 +1,41 @@
-import tensorflow as tf
-from object_detection.utils import dataset_util
-import xml.etree.ElementTree as ET
 import os
+
+import tensorflow as tf
+import xml.etree.ElementTree as ET
+from object_detection.utils import dataset_util
+from PIL import Image
 
 flags = tf.app.flags
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 flags.DEFINE_string('images_dir', '', 'Path to the directory containing images')
 flags.DEFINE_string('labels_dir', '', 'Path to the directory containing labels')
 FLAGS = flags.FLAGS
+
+def create_tf_example_no_annotation(images_dir, filename):
+    image_path = os.path.join(images_dir, filename)
+    im = Image.open(image_path)
+    width, height = im.size
+    image_format = b'jpeg' # b'jpeg' or b'png'
+
+    with tf.gfile.GFile(os.path.join(images_dir, filename), 'rb') as f:
+        encoded_image_data = f.read() # Encoded image bytes
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+      'image/height': dataset_util.int64_feature(height),
+      'image/width': dataset_util.int64_feature(width),
+      'image/filename': dataset_util.bytes_feature(filename.encode('utf-8')),
+      'image/source_id': dataset_util.bytes_feature(filename.encode('utf-8')),
+      'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+      'image/format': dataset_util.bytes_feature(image_format),
+      'image/object/bbox/xmin': dataset_util.float_list_feature([]),
+      'image/object/bbox/xmax': dataset_util.float_list_feature([]),
+      'image/object/bbox/ymin': dataset_util.float_list_feature([]),
+      'image/object/bbox/ymax': dataset_util.float_list_feature([]),
+      'image/object/class/text': dataset_util.bytes_list_feature([]),
+      'image/object/class/label': dataset_util.int64_list_feature([]),
+      'image/object/difficult': dataset_util.int64_list_feature([])
+    }))
+    return tf_example
 
 def create_tf_example(example, images_dir):
     height = int(example[5][1].text) # Image height
@@ -61,12 +89,19 @@ def create_tf_example(example, images_dir):
 def main(_):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
 
-    for filename in os.listdir(FLAGS.labels_dir):
-        xml_path = os.path.join(FLAGS.labels_dir, filename)
-        tree = ET.parse(xml_path)
-        root = tree.getroot()
+    for filename in os.listdir(FLAGS.images_dir):
+        if not '.jpg' in filename:
+            continue
 
-        tf_example = create_tf_example(root, FLAGS.images_dir)
+        xml_path = os.path.join(FLAGS.labels_dir, filename + '.xml')
+
+        if os.path.exists(xml_path):
+            tree = ET.parse(xml_path)
+            root = tree.getroot()
+            tf_example = create_tf_example(root, FLAGS.images_dir)
+        else:
+            tf_example = create_tf_example_no_annotation(FLAGS.images_dir, filename)
+
         writer.write(tf_example.SerializeToString())
 
     writer.close()
