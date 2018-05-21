0ac73226072b37c90a05e512394c33c1e35c63a6
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-05-21 17:54:06 +0200

    Add missing import

--- scripts/inference.py
+++ scripts/inference.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import ops as utils_ops
 from PIL import Image
 from matplotlib import pyplot as plt
 
